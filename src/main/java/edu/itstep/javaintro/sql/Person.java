72f6cf557f186a9e3d71ed42f0635266ece11a7c
User <java.user.tester@gmail.com>
2024-01-07 13:50:38 +0200

    added orm test

--- src/main/java/edu/itstep/javaintro/sql/Person.java
+++ src/main/java/edu/itstep/javaintro/sql/Person.java
@@ -1,7 +1,8 @@
 package edu.itstep.javaintro.sql;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,8 +11,27 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "person")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
+    @Column(name = "first_name")
+    private String name;
 
+    @Column(name = "last_name")
+    private String family_name;
+
+    private int age;
+    private int egn;
+
+    public Person(String name, String family_name, int age, int egn){
+        this.name = name;
+        this.family_name = family_name;
+        this.age = age;
+        this.egn = egn;
+    }
 
 }
---
+++ src/test/java/edu/itstep/javaintro/sql/PersonTest.java
@@ -0,0 +1,52 @@
+package edu.itstep.javaintro.sql;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class PersonTest {
+    private static Logger logger = LoggerFactory.getLogger(PersonTest.class);
+
+    @BeforeEach
+    void setUp() {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        EntityManager em = factory.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Person person = new Person("John", "Doe", 33, 123456);
+            // PersonTest person1 = new PersonTest("John", 20);
+            em.persist(person);
+            // em.persist(person1);
+//        List<Users> result = em.createQuery("from Users").getResultList();
+//        System.out.println("##################################################");
+//        for (Users user : result) {
+//            System.out.println(user.getUserName() + " " + user.getPassword());
+//        }
+            em.getTransaction().commit();
+        } catch (HibernateException he) {
+            em.getTransaction().rollback();
+            logger.error(he.getMessage());
+        }
+        em.close();
+
+    }
+
+    @Test
+    void testPersonName()throws Exception{
+
+    }
+    @AfterEach
+    void tearDown() {
+    }
+}
