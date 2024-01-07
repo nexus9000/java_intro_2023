964caca040525620cb2c3dd3dc52706fa86962d1
User <java.user.tester@gmail.com>
2024-01-07 13:33:38 +0200

    jakarta persistance added

---
+++ src/main/java/edu/itstep/javaintro/sql/Person.java
@@ -0,0 +1,17 @@
+package edu.itstep.javaintro.sql;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Person {
+
+
+
+}
